6cc6579a7e26303928c3b02279c38fce98036a0e
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2020-11-02 18:12:27 -0300

    Add Initial SwapsController and test

---
+++ src/swaps/SwapsController.ts
@@ -0,0 +1,69 @@
+import BaseController, { BaseConfig, BaseState } from '../BaseController';
+
+const METASWAP_ADDRESS = '0x881d40237659c251811cec9c364ef91dc08d300c';
+
+export interface SwapsTokenObject {
+  address: string;
+  symbol: string;
+  decimals: number;
+  occurances?: number;
+  iconUrl?: string;
+}
+
+export interface SwapsConfig extends BaseConfig {
+  maxGasLimit: number;
+  pollCountLimit: number;
+  metaSwapAddress: string;
+}
+
+export enum SwapsError {
+  QUOTES_EXPIRED_ERROR = 'QUOTES_EXPIRED_ERROR',
+  QUOTES_NOT_AVAILABLE_ERROR = 'QUOTES_NOT_AVAILABLE_ERROR',
+}
+
+export interface SwapsState extends BaseState {
+  quotes: {[key: string]: Record<string, any>};
+  fetchParams: null | Record<string, any>;
+  tokens: null | SwapsTokenObject[];
+  quotesLastFetched: null | Date;
+  errorKey: null | SwapsError;
+  topAggId: null | string;
+  swapsFeatureIsLive: boolean;
+}
+
+export default class SwapsController extends BaseController<SwapsConfig, SwapsState> {
+  /**
+   * Name of this controller used during composition
+   */
+  name = 'SwapsController';
+
+  /**
+   * List of required sibling controllers this controller needs to function
+   */
+  requiredControllers = ['NetworkController', 'TokenRatesController'];
+
+  /**
+  * Creates a SwapsController instance
+  *
+  * @param config - Initial options used to configure this controller
+  * @param state - Initial state to set on this controller
+  */
+  constructor(config?: Partial<SwapsConfig>, state?: Partial<SwapsState>) {
+    super(config, state);
+    this.defaultConfig = {
+      maxGasLimit: 2500000,
+      pollCountLimit: 3,
+      metaSwapAddress: METASWAP_ADDRESS,
+    };
+    this.defaultState = {
+      quotes: {},
+      fetchParams: null,
+      tokens: null,
+      quotesLastFetched: null,
+      errorKey: null,
+      topAggId: null,
+      swapsFeatureIsLive: false,
+    };
+    this.initialize();
+  }
+}
---
+++ tests/SwapsController.test.ts
@@ -0,0 +1,26 @@
+import SwapsController from '../src/swaps/SwapsController';
+
+describe('SwapsController', () => {
+
+  it('should set default config', () => {
+    const controller = new SwapsController();
+    expect(controller.config).toEqual({
+      maxGasLimit: 2500000,
+      pollCountLimit: 3,
+      metaSwapAddress: '0x881d40237659c251811cec9c364ef91dc08d300c',
+    });
+  });
+
+  it('should set default state', () => {
+    const controller = new SwapsController();
+    expect(controller.state).toEqual({
+      quotes: {},
+      fetchParams: null,
+      tokens: null,
+      quotesLastFetched: null,
+      errorKey: null,
+      topAggId: null,
+      swapsFeatureIsLive: false,
+    });
+  });
+});
