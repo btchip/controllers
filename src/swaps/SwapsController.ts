a77c43729fc603e1b35896d607909f5f7ec9354d
Esteban Mino <efmino@uc.cl>
2020-11-04 00:55:44 -0300

    init

--- src/swaps/SwapsController.ts
+++ src/swaps/SwapsController.ts
@@ -21,8 +21,12 @@ export enum SwapsError {
   QUOTES_NOT_AVAILABLE_ERROR = 'QUOTES_NOT_AVAILABLE_ERROR',
 }
 
+interface SwapsQuotes {
+  [key: string]: Record<string, any>
+}
+
 export interface SwapsState extends BaseState {
-  quotes: { [key: string]: Record<string, any> };
+  quotes: SwapsQuotes;
   fetchParams: null | Record<string, any>;
   tokens: null | SwapsTokenObject[];
   quotesLastFetched: null | Date;
@@ -31,7 +35,49 @@ export interface SwapsState extends BaseState {
   swapsFeatureIsLive: boolean;
 }
 
+const QUOTE_POLLING_INTERVAL = 50 * 1000
+
 export default class SwapsController extends BaseController<SwapsConfig, SwapsState> {
+  private handle?: NodeJS.Timer;
+  // private pollCount?: number;
+
+  /**
+   * Fetch current gas price
+   * 
+   * @returns - Promise resolving to the current gas price
+   */
+  // private async getEthersGasPrice (): Promise<string> {
+  //   return ''
+  // }
+
+  /**
+   * Find best quote and savings from specific quotes
+   * 
+   * @param quotes - Quotes to do the calculation
+   * @returns - Promise resolving to an object containing best aggregator id and respective savings
+   */
+  // private async findTopQuoteAndCalculateSavings (quotes: SwapsQuotes): Promise<Object> {
+  //   Object.keys(quotes)
+  //   return {topAggId: '', isBest: true, savings: {}}
+  // }
+
+  /**
+   * Get current allowance for a wallet address to access ERC20 contract address funds
+   * 
+   * @param _contractAddress - Hex address of the ERC20 contract
+   * @param _walletAddress - Hex address of the wallet
+   * @returns
+   */
+  // private async getERC20Allowance (_contractAddress: string, _walletAddress: string): Promise<number> {
+  //   return 0
+  // }
+
+  // private _setupSwapsLivenessFetching () {}
+
+  // private async _fetchAndSetSwapsLiveness () {}
+
+  // private async _modifyValuesForMaxEthMode (newQuotes, accountBalance) {}
+  
   /**
    * Name of this controller used during composition
    */
@@ -66,4 +112,106 @@ export default class SwapsController extends BaseController<SwapsConfig, SwapsSt
     };
     this.initialize();
   }
+
+  setSwapsTokens (newTokens: null | SwapsTokenObject[]) {
+    this.update({ tokens: newTokens })
+  }
+
+  setSwapsErrorKey (newErrorKey: null | SwapsError) {
+    this.update({ errorKey: newErrorKey })
+  }
+
+  setQuotesLastFetched (quotesLastFetched: SwapsQuotes) {
+    this.update({ quotes: quotesLastFetched })
+  }
+
+  setSwapsLiveness (isLive: boolean) {
+    this.update({ swapsFeatureIsLive: isLive })
+  }
+
+  /**
+   * Starts a new polling process
+   * 
+   */
+  pollForNewQuotes() {
+    this.handle && clearTimeout(this.handle);
+    this.fetchAndSetQuotes(null, {}, true)
+    this.handle = setTimeout(() => {
+      this.fetchAndSetQuotes(null, {}, true)
+    }, QUOTE_POLLING_INTERVAL);
+  }
+
+  /**
+   * Stops the polling process
+   * 
+   */
+  stopPollingForQuotes() {
+    this.handle && clearTimeout(this.handle)
+  }
+
+  async fetchAndSetQuotes (fetchParams: null | Record<string, any>, fetchParamsMetaData: Object, isPolledRequest: boolean) {
+    if (!fetchParams) {
+      return null
+    }
+
+    // Every time we get a new request that is not from the polling, we reset the poll count so we can poll for up to three more sets of quotes with these new params.
+    if (!isPolledRequest) {
+      // this.pollCount = 0
+    }
+
+    // If there are any pending poll requests, clear them so that they don't get call while this new fetch is in process
+    this.handle && clearTimeout(this.handle)
+
+    if (!isPolledRequest) {
+      this.setSwapsErrorKey(null)
+    }
+    // ...
+
+    this.update({fetchParams: { ...fetchParams, metaData: fetchParamsMetaData }})
+  }
+
+  safeRefetchQuotes () {
+    const {fetchParams} = this.state
+    if (!this.handle && fetchParams) {
+      this.fetchAndSetQuotes(fetchParams, {}, false)
+    }
+  }
+
+  async getAllQuotesWithGasEstimates (quotes: SwapsQuotes):   Promise<SwapsQuotes> {
+    const newQuotes = quotes
+    return newQuotes
+  }
+
+  resetPostFetchState () {
+    const {tokens: resetTokens, fetchParams: resetFetchParams, swapsFeatureIsLive: resetSwapsFeatureIsLive} = this.state
+    this.update({
+      ...this.state,
+      tokens: resetTokens,
+      fetchParams: resetFetchParams,
+      swapsFeatureIsLive: resetSwapsFeatureIsLive
+    })
+    this.handle && clearTimeout(this.handle)
+  }
+
+  // resetSwapsState () {}
+
+  // timedoutGasReturn (tradeTxParams) {}
+
+  // setSelectedQuoteAggId (selectedAggId) () {}
+
+  // async setInitialGasEstimate (initialAggId, baseGasEstimate) {}
+
+  // setApproveTxId (approveTxId) {}
+
+  // setTradeTxId (tradeTxId) {}
+
+  // setMaxMode (maxMode) {}
+
+  // setSwapsTxGasPrice (gasPrice) {}
+
+  // setSwapsTxGasLimit (gasLimit) {}
+
+  // setCustomApproveTxData (data) {}
+
+  // setBackgroundSwapRouteState (routeState) {}
 }
